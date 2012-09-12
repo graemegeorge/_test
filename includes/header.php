122fe51ad1c7f004b0a4256782a02f634d7bc09b
Graeme George <graemelgeorge@gmail.com>
2012-09-12 14:37:21 +0100

    Now using PHP, includes etc

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>  
+<html class="no-js">  
+
+<head>  
+<meta charset="utf-8">  
+
+<title>A new direction</title>  
+  
+<script src="http://modernizr.com/downloads/modernizr.js" charset="utf-8"></script>
+
+<link rel="stylesheet" href="css/screen.css">
+
+</head>  
+
+<body>  
+
+  <div class="container">
+  	
+  	<header>
+	  
+	  <nav>
+	  
+	    <ul>
+	    
+	      <li><a href="index.php" title="home">Home</a></li>
+	      
+	      <li><a href="page.php" title="Go to page two">More</a></li>
+	      
+	      <li><a href="http://www.google.com" title="Visit google.com">Search..</a></li>
+	    
+	    </ul>
+	  
+	  </nav>
+
+  	</header>
